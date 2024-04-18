8a923e247c92402fac1d500ba2b6245dc86a9abf
João Matheus <138176279+joaomatheusm@users.noreply.github.com>
2024-04-18 20:25:38 -0300

    dark-mode criado

---
+++ dark-mode/script.js
@@ -0,0 +1,16 @@
+// 14 - Botao "dark mode" que muda o fundo para preto e a letra para branco e seu texto para "dark mode", e vice versa
+
+const fundo = document.querySelector('#fundo');
+const btn_darkmode = document.querySelector('#dark-mode');
+const body = document.querySelector('#fundo-body');
+
+btn_darkmode.addEventListener('click', () => {
+    fundo.classList.toggle('dark-mode');
+    body.classList.toggle('body-dark');
+
+    if (btn_darkmode.innerHTML === 'Dark Mode') {
+        btn_darkmode.innerHTML = 'Light Mode';
+    } else {
+        btn_darkmode.innerHTML = 'Dark Mode';
+    }
+});
